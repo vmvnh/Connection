82ba24b0ded2f3d8bb822e7c4e73a623e6aa23cf
Yaroslav <yaroslav-w@mail.ru>
2022-08-25 22:45:54 +0400

    feat: Добавлено получение идентификатора пользователя.

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,5 +1,20 @@
+import data.Failures;
+import services.HttpService;
+import java.util.Objects;
+import java.util.Scanner;
+
 public final class Application {
     public static void main(String[] args) {
-
+        var result = "";
+        try {
+            Scanner scan = new Scanner(System.in);
+            var userId = scan.nextInt();
+            scan.close();
+            result = HttpService.getUserWithUrlConnection(userId);
+        } catch (Exception e) {
+            System.err.println(Failures.INVALID_ARGUMENT);
+        }
+        if (!Objects.equals(result, ""))
+            System.out.println(result);
     }
 }
