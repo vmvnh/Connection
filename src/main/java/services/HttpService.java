96049b31940f2dbc5da09cbaa229a5f0472fb6a0
Yaroslav <yaroslav-w@mail.ru>
2022-08-28 00:02:52 +0400

    fix: Изменен класс подключения и исправлен стиль кода.

--- src/main/java/services/HttpService.java
+++ src/main/java/services/HttpService.java
@@ -1,7 +1,5 @@
 package services;
 
-import data.Failures;
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -9,38 +7,38 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.stream.Collectors;
 
+/**
+ * Подключение к сайту.
+ */
 public final class HttpService {
-    /**
-     * Шаблон адреса для получения данных пользователя на сервисе Reqres.
-     */
-    public static final String BASE_URL = "https://reqres.in/";
-    public static final String USER_URI = "api/users/";
-    public static String url = BASE_URL + USER_URI;
+  /**
+   * Шаблон адреса для получения данных пользователя на сервисе Reqres.
+   */
+  public static final String BASE_URL = "https://reqres.in/";
+  public static final String USER_URI = "api/users/";
+  public static String url = BASE_URL + USER_URI;
 
-    /**
-     * Получение пользователя по идентификатору.
-     *
-     * @param id Идентификатор пользователя.
-     * @return Данные пользователя или ошибку.
-     */
-    public static String getUserWithUrlConnection(int id) throws IOException {
-        var url = new URL(HttpService.url + id);
-        var connection = (HttpURLConnection) url.openConnection();
-        var responseCode = connection.getResponseCode();
-        switch (responseCode) {
-            case 404 -> {
-                System.err.println(Failures.NOT_FOUND);
-                return null;
-            }
-            case 200 -> {
-                return new BufferedReader(new InputStreamReader(connection.getInputStream()))
-                        .lines()
-                        .collect(Collectors.joining());
-            }
-            default -> {
-                System.err.println(Failures.OTHER_FAILURES);
-                return null;
-            }
-        }
-    }
+  /**
+   * Свойство объекта с данными пользователя, содержащее имя пользователя.
+  */
+  public static final String FIRST_NAME = "first_name";
+
+  /**
+   * Свойство объекта с данными пользователя, содержащее фамилию пользователя.
+   */
+  public static final String LAST_NAME = "last_name";
+
+  /**
+   * Получение пользователя по идентификатору.
+   *
+   * @param id Идентификатор пользователя.
+   * @return Данные пользователя или ошибку.
+   */
+  public static String getUserWithUrlConnection(int id) throws IOException {
+    var url = new URL(HttpService.url + id);
+    var connection = (HttpURLConnection) url.openConnection();
+    return new BufferedReader(new InputStreamReader(connection.getInputStream()))
+        .lines()
+        .collect(Collectors.joining());
+  }
 }
